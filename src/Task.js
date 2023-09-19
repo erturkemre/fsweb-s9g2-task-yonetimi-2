b4d8e7c81630310fb743f699aa7eb099478eb836
Emre <emre.ertrk.3455@gmail.com>
2023-09-19 21:28:33 +0300

    tailwind implemented

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from "date-fns";
+import {tr} from 'date-fns/locale';
 
 const Task = ({ taskObj, onComplete }) => {
+  const tarih = new Date(taskObj.deadline);
+  const kalanGun = formatDistanceToNow(tarih, {
+    addSuffix: true,
+    locale: tr,
+  });
+  const accentClass = differenceInDays(tarih, new Date()) <= 3 ?  'normal' : 'urgent';
 
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
-      <p>{taskObj.description}</p>
+    <div className="p-6 bg-white rounded-md leading-normal mt-4 shadow-[0_4px_5px_0px_rgba(0,0,0,0.10)]">
+      <h3 className="text-lg text-task">{taskObj.title}</h3>
+      <div className="text-xs pt-1">
+        son teslim: <span className={`${accentClass} px-2 py-1 inline-block rounded-sm`}>{kalanGun}</span>
+      </div>
+      <p className="pt-2 pb-3 px-0 text-sm text-[#444]">{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="inline-block py-1.5 px-3 text-sm border-solid border-2 border-pill rounded-3xl mr-1 mb-1.5" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button className="block py-[5px] px-3 ml-auto bg-[#fecc91] shadow-[0_4px_5px_0px_rgb(0,0,0,0.05)] rounded-1 border-0 cursor-pointer  " onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
